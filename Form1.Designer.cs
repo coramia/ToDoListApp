2c3167ca48f20a539f15f59fb475228bdb4940af
coramiam <coramia@gmail.com>
2025-09-29 16:24:16 +0300

    To Do List simple app: create, edit, delete list items

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,72 +29,166 @@
         private void InitializeComponent()
         {
             label1 = new Label();
-            textBox1 = new TextBox();
-            textBox2 = new TextBox();
+            titleTextBox = new TextBox();
+            descriptionTextBox = new TextBox();
             label2 = new Label();
             label3 = new Label();
+            newButton = new Button();
+            editButton = new Button();
+            deleteButton = new Button();
+            saveButton = new Button();
+            toDoListView = new DataGridView();
+            label4 = new Label();
+            label5 = new Label();
+            ((System.ComponentModel.ISupportInitialize)toDoListView).BeginInit();
             SuspendLayout();
             // 
             // label1
             // 
+            label1.BackColor = Color.PowderBlue;
             label1.Font = new Font("Book Antiqua", 36F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label1.Location = new Point(25, 0);
+            label1.ForeColor = Color.Black;
+            label1.Location = new Point(25, 35);
             label1.Name = "label1";
-            label1.Size = new Size(1719, 104);
+            label1.Size = new Size(393, 104);
             label1.TabIndex = 0;
             label1.Text = "To Do List";
-            label1.TextAlign = ContentAlignment.TopCenter;
             label1.Click += label1_Click;
             // 
-            // textBox1
+            // titleTextBox
             // 
-            textBox1.Location = new Point(25, 189);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(1719, 31);
-            textBox1.TabIndex = 1;
+            titleTextBox.Location = new Point(25, 189);
+            titleTextBox.Name = "titleTextBox";
+            titleTextBox.Size = new Size(1719, 31);
+            titleTextBox.TabIndex = 1;
             // 
-            // textBox2
+            // descriptionTextBox
             // 
-            textBox2.Location = new Point(25, 282);
-            textBox2.Name = "textBox2";
-            textBox2.Size = new Size(1719, 31);
-            textBox2.TabIndex = 2;
+            descriptionTextBox.Location = new Point(25, 282);
+            descriptionTextBox.Name = "descriptionTextBox";
+            descriptionTextBox.Size = new Size(1719, 31);
+            descriptionTextBox.TabIndex = 2;
             // 
             // label2
             // 
-            label2.Font = new Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label2.Location = new Point(25, 239);
+            label2.Font = new Font("Book Antiqua", 10F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(25, 252);
             label2.Name = "label2";
-            label2.Size = new Size(1719, 38);
+            label2.Size = new Size(1719, 27);
             label2.TabIndex = 3;
             label2.Text = "Description:";
             label2.Click += label2_Click;
             // 
             // label3
             // 
-            label3.Font = new Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label3.Location = new Point(25, 139);
+            label3.Font = new Font("Book Antiqua", 10F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.Location = new Point(25, 156);
             label3.Name = "label3";
-            label3.Size = new Size(1719, 38);
+            label3.Size = new Size(1719, 30);
             label3.TabIndex = 4;
             label3.Text = "Title:";
             label3.Click += label3_Click;
+            // 
+            // newButton
+            // 
+            newButton.Font = new Font("Book Antiqua", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            newButton.Location = new Point(25, 359);
+            newButton.Name = "newButton";
+            newButton.Size = new Size(377, 74);
+            newButton.TabIndex = 5;
+            newButton.Text = "New";
+            newButton.UseVisualStyleBackColor = true;
+            newButton.Click += newButton_Click;
+            // 
+            // editButton
+            // 
+            editButton.Font = new Font("Book Antiqua", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            editButton.Location = new Point(470, 359);
+            editButton.Name = "editButton";
+            editButton.Size = new Size(377, 74);
+            editButton.TabIndex = 6;
+            editButton.Text = "Edit";
+            editButton.UseVisualStyleBackColor = true;
+            editButton.Click += editButton_Click;
+            // 
+            // deleteButton
+            // 
+            deleteButton.Font = new Font("Book Antiqua", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            deleteButton.Location = new Point(913, 359);
+            deleteButton.Name = "deleteButton";
+            deleteButton.Size = new Size(377, 74);
+            deleteButton.TabIndex = 7;
+            deleteButton.Text = "Delete";
+            deleteButton.UseVisualStyleBackColor = true;
+            deleteButton.Click += deleteButton_Click;
+            // 
+            // saveButton
+            // 
+            saveButton.Font = new Font("Book Antiqua", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            saveButton.Location = new Point(1367, 359);
+            saveButton.Name = "saveButton";
+            saveButton.Size = new Size(377, 74);
+            saveButton.TabIndex = 8;
+            saveButton.Text = "Save";
+            saveButton.UseVisualStyleBackColor = true;
+            saveButton.Click += saveButton_Click;
+            // 
+            // toDoListView
+            // 
+            toDoListView.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            toDoListView.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
+            toDoListView.BackgroundColor = Color.White;
+            toDoListView.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            toDoListView.Location = new Point(25, 452);
+            toDoListView.Name = "toDoListView";
+            toDoListView.RowHeadersWidth = 62;
+            toDoListView.Size = new Size(1719, 567);
+            toDoListView.TabIndex = 9;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Book Antiqua", 11F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label4.Location = new Point(470, 82);
+            label4.Name = "label4";
+            label4.Size = new Size(711, 27);
+            label4.TabIndex = 10;
+            label4.Text = "Make all your productivity dreams come true with the click of a button.\r\n";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Book Antiqua", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label5.Location = new Point(391, 35);
+            label5.Name = "label5";
+            label5.Size = new Size(40, 22);
+            label5.TabIndex = 11;
+            label5.Text = "TM";
+            label5.Click += label5_Click;
             // 
             // ToDoList
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            BackColor = Color.PaleTurquoise;
-            ClientSize = new Size(1771, 1320);
+            BackColor = Color.PowderBlue;
+            ClientSize = new Size(1771, 1041);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(toDoListView);
+            Controls.Add(saveButton);
+            Controls.Add(deleteButton);
+            Controls.Add(editButton);
+            Controls.Add(newButton);
             Controls.Add(label3);
             Controls.Add(label2);
-            Controls.Add(textBox2);
-            Controls.Add(textBox1);
+            Controls.Add(descriptionTextBox);
+            Controls.Add(titleTextBox);
             Controls.Add(label1);
             ForeColor = Color.Black;
             Name = "ToDoList";
             Text = "To Do List";
-            Load += Form1_Load;
+            Load += ToDoList_Load;
+            ((System.ComponentModel.ISupportInitialize)toDoListView).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -102,9 +196,16 @@
         #endregion
 
         private Label label1;
-        private TextBox textBox1;
-        private TextBox textBox2;
+        private TextBox titleTextBox;
+        private TextBox descriptionTextBox;
         private Label label2;
         private Label label3;
+        private Button newButton;
+        private Button editButton;
+        private Button deleteButton;
+        private Button saveButton;
+        private DataGridView toDoListView;
+        private Label label4;
+        private Label label5;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Data;
+
 namespace ToDoList
 {
     public partial class ToDoList : Form
@@ -7,9 +10,18 @@ namespace ToDoList
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
-        {
+        DataTable todoList = new DataTable();
+        bool isEditing = false;
 
+
+        private void ToDoList_Load(object sender, EventArgs e)
+        {
+            // Create column
+            todoList.Columns.Add("Title");
+            todoList.Columns.Add("Description");
+
+            // point our datagridview to out datasource
+            toDoListView.DataSource = todoList;
         }
 
         private void label1_Click(object sender, EventArgs e)
@@ -23,6 +35,70 @@ namespace ToDoList
         }
 
         private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void newButton_Click(object sender, EventArgs e)
+        {
+            // everytime we click new, we clear the text fields
+            titleTextBox.Text = "";
+            descriptionTextBox.Text = "";
+        }
+
+        private void editButton_Click(object sender, EventArgs e)
+        {
+            isEditing = true;
+
+            // Fill text fields with data from table
+            titleTextBox.Text = todoList.Rows[toDoListView.CurrentCell.RowIndex].ItemArray[0].ToString();
+            descriptionTextBox.Text = todoList.Rows[toDoListView.CurrentCell.RowIndex].ItemArray[1].ToString();
+        }
+
+        private void deleteButton_Click(object sender, EventArgs e)
+        {
+            // the try catch is added to avoid scenarios where the actual item may or may not be present there, and to avoid crashes
+            try
+            {
+                // to delete the entire row
+                todoList.Rows[toDoListView.CurrentCell.RowIndex].Delete();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error:" + ex);
+            }
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            if (isEditing)
+            {
+                //if the entry already exists inside the grid, then use it to update the title and descriptions
+                todoList.Rows[toDoListView.CurrentCell.RowIndex]["Title"] = titleTextBox.Text;
+                todoList.Rows[toDoListView.CurrentCell.RowIndex]["Description"] = descriptionTextBox.Text;
+            }
+            else
+            {
+                todoList.Rows.Add(titleTextBox.Text, descriptionTextBox.Text);
+            }
+
+            // clear out fields after save
+            titleTextBox.Text = "";
+            descriptionTextBox.Text = "";
+            isEditing = false;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
         {
 
         }
